3855313721baeb7a44b483bb4792aefd72555c89
Seth Treviño <sethtv@hotmail.com>
2019-02-17 19:57:42 -0600

    add Readme, LICENSE, gitignore

---
+++ src/fbcmd4j/Fbcmd4j.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fbcmd4j;
+
+/**
+ *
+ * @author sethtv10
+ */
+public class Fbcmd4j {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
